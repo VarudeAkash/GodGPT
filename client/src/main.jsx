792ae0a90706a831c27e9f5ce1ccd470ea0a5350
Akash Shivaji Varude <sky@Akashs-MacBook-Air.local>
2025-10-25 15:51:13 +0530

    added import statement for analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Analytics } from '@vercel/analytics/react'  // ADD THIS LINE
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
